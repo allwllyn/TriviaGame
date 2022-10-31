c12c6bbcf05ee28b2e218022c2eef63465e8d31e
nateej <113387075+nateej@users.noreply.github.com>
2022-10-31 15:39:08 -0400

    Add files via upload

---
+++ css/about.php
@@ -0,0 +1,25 @@
+<?php session_start();
+
+if(!isset($_SESSION['name'])){
+  header("location:login.php");
+  exit;
+}
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+ <link rel="stylesheet" type ="text/css" href="about.css"/></head>
+<body>
+
+<h2>Panther Trivia</h2>
+<h2>About</h2>
+<div class="about">
+
+ <p> This is a trivia game made entirely with PHP, HTML, and CSS. </p>
+ <p> First you must create a login, then select the difficulty setting.</p>
+ <p> After this, simply answer the questions and try to get the high score!</p>
+</div>
+<img src="about.jpg" alt="Trivia">
+</body>
+</html>
